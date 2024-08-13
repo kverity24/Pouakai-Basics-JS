9a7b522937ac77c4bb52f9dc8782593202df686f
Kharma Verity <k.verity@ngenroom.co.nz>
2024-08-13 14:56:26 +1200

    initial commit

--- JS/basics.js
+++ JS/basics.js
@@ -1,28 +1,28 @@
 // basics.js
 
 // 1. Write a variable named myName and make it a string of your name
-
+var myName = 'Kharma'
 
 // 2. Write a variable named age and make it a number of your age
-
+var age = 24
 
 // 3. Write a variable named isStudent and make it a boolean that represents your status as a student
-
+var isStudent = true
 
 // 4. Write a variable named favoriteColor and leave it undefined
-
+var favoriteColor
 
 // 5. Write a variable named greeting and set it to "Hello, world!"
-
+var greeting = 'Hello, world!'
 
 // 6. Write a variable named myUndefined and leave it undefined
-
+var myUndefined
 
 // 7. Write a variable named myFavColor and make it a string of your favorite color
-
+var myFavColor = "Red"
 
 // 8. Define your favoriteColor variable as myFavColor
-
+favoriteColor = myFavColor
 
 
 
